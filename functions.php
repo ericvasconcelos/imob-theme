f1b550d3fade2b56133913d635580daba3ce67a0
Eric Vasconcelos de Oliveira <eric_vasconcelosoliveira@hotmail.com>
2015-01-11 01:29:39 -0200

    Criando Post Type do Carrossel

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
 // require_once get_template_directory() . '/core/classes/class-thumbnail-resizer.php';
 require_once get_template_directory() . '/core/classes/class-theme-options.php';
 // require_once get_template_directory() . '/core/classes/class-options-helper.php';
-// require_once get_template_directory() . '/core/classes/class-post-type.php';
+require_once get_template_directory() . '/core/classes/class-post-type.php';
 // require_once get_template_directory() . '/core/classes/class-taxonomy.php';
 // require_once get_template_directory() . '/core/classes/class-metabox.php';
 // require_once get_template_directory() . '/core/classes/abstracts/abstract-front-end-form.php';
@@ -504,3 +504,24 @@ add_action( 'init', 'odin_theme_settings', 1 );
 // }
 // add_filter('intermediate_image_sizes_advanced', 'wpmayor_filter_image_sizes');
  
+
+ function odin_carousel_cpt() {
+    $video = new Odin_Post_Type(
+        'Carrossel', // Nome (Singular) do Post Type.
+        'carrossel' // Slug do Post Type.
+    );
+
+    $video->set_labels(
+        array(
+            'menu_name' => __( 'Carrossel', 'odin' )
+        )
+    );
+
+    $video->set_arguments(
+        array(
+            'supports' => array( 'title' )
+        )
+    );
+}
+
+add_action( 'init', 'odin_carousel_cpt', 1 );
